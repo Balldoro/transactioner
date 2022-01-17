945b1b439f5608fd34ead708657d57aaafb89cd9
Marek Miklaszewski <mar.mik1@wp.pl>
2022-01-18 00:33:10 +0100

    fix(controlwrapper): extend FormControlProps in ControlWrapper
    40

--- src/components/Inputs/ControlWrapper/index.tsx
+++ src/components/Inputs/ControlWrapper/index.tsx
@@ -1,14 +1,24 @@
-import { FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormControlProps,
+} from "@chakra-ui/react";
 
-type ControlWrapperProps = {
+type ControlWrapperProps = FormControlProps & {
   label: string;
   error?: string;
   children: React.ReactNode;
 };
 
-const ControlWrapper = ({ label, error, children }: ControlWrapperProps) => {
+const ControlWrapper = ({
+  label,
+  error,
+  children,
+  ...props
+}: ControlWrapperProps) => {
   return (
-    <FormControl isInvalid={Boolean(error)}>
+    <FormControl isInvalid={Boolean(error)} {...props}>
       <FormLabel>{label}</FormLabel>
       {children}
       {error && <FormErrorMessage>{error}</FormErrorMessage>}
