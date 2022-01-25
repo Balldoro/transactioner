050035355356dbf53f9f754e65f30ff9cb56bbc2
Marek Miklaszewski <mar.mik1@wp.pl>
2022-01-25 19:42:49 +0100

    Feat/56 transaction item (#59)
    * feat(transactions): add id's to transactions, change category translation
    
    56
    
    * feat(transactions): create TransactionItem
    
    56

--- src/modules/category/components/Transactions/index.tsx
+++ src/modules/category/components/Transactions/index.tsx
@@ -1,29 +1,47 @@
-import { Button, Flex, Heading, VStack } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, VStack } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
+
+import { FullGroup } from "modules/category/types";
 import EmptyTransactions from "../EmptyTransactions";
+import TransactionItem from "../TransactionItem";
 
 type TransactionsProps = {
-  group: any;
+  group: FullGroup;
 };
 
 const Transactions = ({ group }: TransactionsProps) => {
   const { t } = useTranslation("category");
 
   return (
-    <VStack spacing="4" align="flex-start">
-      <Flex
-        align={{ md: "center" }}
-        justify="space-between"
-        width={{ base: "100%", sm: "auto", md: "100%" }}
-        direction={{ base: "column", md: "row" }}>
-        <Heading fontWeight="medium" fontSize="3xl" mb={{ base: "4", md: "0" }}>
-          {t("transactions")}
-        </Heading>
-        <Button colorScheme="blue">{t("create-new-transaction")}</Button>
-      </Flex>
+    <Box overflowX="auto" width={{ sm: "100%", lg: "45%" }}>
+      <VStack spacing="4" align="flex-start">
+        <Flex
+          align={{ md: "center" }}
+          justify="space-between"
+          width={{ base: "100%", sm: "auto", md: "100%" }}
+          direction={{ base: "column", md: "row" }}>
+          <Heading
+            fontWeight="medium"
+            fontSize="3xl"
+            mb={{ base: "4", md: "0" }}>
+            {t("transactions")}
+          </Heading>
+          <Button colorScheme="blue">{t("create-transaction")}</Button>
+        </Flex>
 
-      {group.transactions.length === 0 && <EmptyTransactions />}
-    </VStack>
+        {group.transactions.length === 0 ? (
+          <EmptyTransactions />
+        ) : (
+          group.transactions.map(transaction => (
+            <TransactionItem
+              key={transaction.id}
+              transaction={transaction}
+              currency={group.currency}
+            />
+          ))
+        )}
+      </VStack>
+    </Box>
   );
 };
 export default Transactions;
