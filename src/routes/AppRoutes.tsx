9c2016f04420d046c4f89207c916549d76a57843
Marek Miklaszewski <mar.mik1@wp.pl>
2022-01-30 18:36:32 +0100

    feat(lazyload): add lazy page imports
    68

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,28 +1,35 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import PrivateRoute from "./PrivateRoute";
 import { RoutePaths } from "./RoutePaths";
-import Dashboard from "modules/dashboard/pages/Dashboard";
 import Layout from "components/Layout";
-import CategoryOverview from "modules/category/pages/CategoryOverview";
+import LoadingSpinner from "components/LoadingSpinner";
+
+const Dashboard = lazy(() => import("modules/dashboard/pages/Dashboard"));
+const CategoryOverview = lazy(
+  () => import("modules/category/pages/CategoryOverview")
+);
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path={RoutePaths.BASE} element={<PrivateRoute />}>
-        <Route path={RoutePaths.BASE} element={<Layout />}>
-          <Route path={RoutePaths.BASE} element={<Dashboard />} />
-          <Route path={RoutePaths.DASHBOARD} element={<Dashboard />} />
-          <Route path={RoutePaths.GROUPS} element={<Dashboard />} />
-          <Route path={RoutePaths.PROFILE} element={<Dashboard />} />
-          <Route path={RoutePaths.SETTINGS} element={<Dashboard />} />
-          <Route
-            path={`${RoutePaths.GROUP}/:id`}
-            element={<CategoryOverview />}
-          />
+    <Suspense fallback={<LoadingSpinner isFullPage />}>
+      <Routes>
+        <Route path={RoutePaths.BASE} element={<PrivateRoute />}>
+          <Route path={RoutePaths.BASE} element={<Layout />}>
+            <Route path={RoutePaths.BASE} element={<Dashboard />} />
+            <Route path={RoutePaths.DASHBOARD} element={<Dashboard />} />
+            <Route path={RoutePaths.GROUPS} element={<Dashboard />} />
+            <Route path={RoutePaths.PROFILE} element={<Dashboard />} />
+            <Route path={RoutePaths.SETTINGS} element={<Dashboard />} />
+            <Route
+              path={`${RoutePaths.GROUP}/:id`}
+              element={<CategoryOverview />}
+            />
+          </Route>
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </Suspense>
   );
 };
 export default AppRoutes;
